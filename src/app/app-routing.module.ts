570b0c851feb76aa23ecf486c7c1c139f8e80399
lpn-leandro <pnunesleandro@gmail.com>
2023-10-03 15:35:06 -0300

    landing-page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
-  //{ path: ''}
+  { path: 'navbar-component', component: NavbarComponent},
+  
 ];
 
 @NgModule({
