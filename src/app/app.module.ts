8d3a3c8a7c2376c11dd147769ce3ea1e3a925db3
erickwiatkowski <erickwiatkowski4@gmail.com>
2023-10-10 19:43:57 -0300

    concluido passo 1 do registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
