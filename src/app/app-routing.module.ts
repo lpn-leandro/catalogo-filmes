7584e0cf0c8d5c27438d7cdd27d15579ef5eed10
erickwiatkowski <erickwiatkowski4@gmail.com>
2023-10-17 18:58:57 -0300

    'minor fixes'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { RegisterComponent } from './register/register.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
