c940f4d12fb2742ad89d367845a2c40e85c63f01
lpn-leandro <pnunesleandro@gmail.com>
2023-11-16 21:45:32 -0300

    'readme update'

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -3,14 +3,19 @@ import { RouterModule } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../movie.service';
 import { Movie } from '../movie'
-
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {DatePipe} from '@angular/common';
+import {MatListModule} from '@angular/material/list';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.scss'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,MatListModule, MatIconModule, MatDividerModule, DatePipe, MatButtonModule, MatTooltipModule],
 })
 export class MovieDetailComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
